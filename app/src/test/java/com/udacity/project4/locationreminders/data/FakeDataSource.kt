c5c2b60d515ee2cd19e573a7d9ba0a0e729372a9
Mo salah <MSalahElrayes@gmail.com>
2022-10-14 11:21:55 +0200

    testing

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -2,27 +2,61 @@ package com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(private var remindersList: MutableList<ReminderDTO> = mutableListOf()) :
+    ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    val reminderData = ReminderDataItem(
+        title = "Title",
+        description = "Description",
+        location = "Location",
+        latitude = 45.toDouble(),
+        longitude = 12.toDouble()
+    )
 
-    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
-    }
+    val reminderDTO = ReminderDTO(
+        title = "Title",
+        description = "Description",
+        location = "Location",
+        latitude = 45.toDouble(),
+        longitude = 12.toDouble()
+    )
 
-    override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+    var shouldReturnError = false
+
+    fun shouldReturnError(shouldReturnError: Boolean) {
+        this.shouldReturnError = shouldReturnError
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        val chosenReminder = remindersList.find { it.id == id }
+
+        return if (shouldReturnError) {
+            Result.Error("Error")
+        } else {
+            if (chosenReminder != null) {
+                Result.Success(chosenReminder)
+            } else {
+                Result.Error("Item not found.")
+            }
+        }
+    }
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        return if (shouldReturnError) {
+            Result.Error("Error")
+        } else {
+            Result.Success(remindersList)
+        }
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        remindersList.add(reminder)
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        remindersList.clear()
     }
-
-
 }
